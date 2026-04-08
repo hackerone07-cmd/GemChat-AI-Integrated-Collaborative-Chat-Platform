c9d261e08aa379772dc56fa6abd85f53c4a42ac0
aryan <bhatiaaryan587@gmail.com>
2026-04-08 12:57:52 +0530

    remove the package

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,7 +3,7 @@ import AppRoute from "./Routes/AppRoute";
 import { UserProvider } from "./Context/user.context";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {Analytics } from "@vercel/analytics"
+
 
 const App = () => (
   <UserProvider>
@@ -18,7 +18,7 @@ const App = () => (
       draggable
       theme="dark"
     />
-    <Analytics/>
+   
   </UserProvider>
 );
 
